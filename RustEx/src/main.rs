22875dd5650b82bbd9fa7be90f04d249111c71aa
Janek <gumzub1@gmail.com>
2025-04-06 11:22:34 +0200

    init

--- RustEx/src/main.rs
+++ RustEx/src/main.rs
@@ -1,11 +1,15 @@
+use std::io::stdin;
+
 struct Automat{
-    nodes: Vec<[u32, 26]>;
-    starting: Vec<u32>;
-    ending: Vec<bool>;
+    nodes: Vec<[u32; 26]>,
+    starting: Vec<u32>,
+    ending: Vec<bool>,
 }
 
-
-
 fn main() {
+    let input=stdin();
+    let mut string=String::new();
+
+    input.read_line(&mut string).expect("error");
     println!("Hello, world!");
 }
